56610383eb5ac551b2ebd40ec4857f7c157bc847
Tuba <tubacayir4545@gmail.com>
2026-07-07 18:02:05 +0300

    login

--- frontend/src/pages/CustomerProfilePage.tsx
+++ frontend/src/pages/CustomerProfilePage.tsx
@@ -14,7 +14,7 @@ import {
     UserRound,
   } from "lucide-react";
   import { Link } from "react-router-dom";
-  
+
   import { useAuthStore } from "../features/auth/authStore";
   import { useCustomerStore } from "../store/customerStore";
   import { useInvoiceStore } from "../store/invoiceStore";
